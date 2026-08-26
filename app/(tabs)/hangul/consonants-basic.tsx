58d9f6ab00709958344c91f5f0e06f0eff3e3995
arben7560 <peposi.arben60@gmail.com>
2026-08-26 18:40:28 +0200

    Apply Hangul editorial copy to consonant lessons

--- app/(tabs)/hangul/consonants-basic.tsx
+++ app/(tabs)/hangul/consonants-basic.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 import { HangulLessonScreen } from "../../../components/hangul/HangulLessonScreen";
+import { applyHangulEditorialOverrides } from "../../../data/hangul/editorialOverrides";
+
+applyHangulEditorialOverrides();
 
 export default function ConsonantsBasicScreen() {
   return <HangulLessonScreen moduleId="hangul_consonants_basic" />;
