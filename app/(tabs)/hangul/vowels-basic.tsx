1a9ff4bbf71741153970d613bf11202a6b143cfd
arben7560 <peposi.arben60@gmail.com>
2026-08-26 18:40:21 +0200

    Apply Hangul editorial copy to vowel lessons

--- app/(tabs)/hangul/vowels-basic.tsx
+++ app/(tabs)/hangul/vowels-basic.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 import { HangulLessonScreen } from "../../../components/hangul/HangulLessonScreen";
+import { applyHangulEditorialOverrides } from "../../../data/hangul/editorialOverrides";
+
+applyHangulEditorialOverrides();
 
 export default function VowelsBasicScreen() {
   return <HangulLessonScreen moduleId="hangul_vowels_basic" />;
