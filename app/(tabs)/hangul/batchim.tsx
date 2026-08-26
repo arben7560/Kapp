86080f467e1e7dc17c9a415d9d3cc0d8c392283c
arben7560 <peposi.arben60@gmail.com>
2026-08-26 18:40:47 +0200

    Apply Hangul editorial copy to batchim lessons

--- app/(tabs)/hangul/batchim.tsx
+++ app/(tabs)/hangul/batchim.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 import { HangulLessonScreen } from "../../../components/hangul/HangulLessonScreen";
+import { applyHangulEditorialOverrides } from "../../../data/hangul/editorialOverrides";
+
+applyHangulEditorialOverrides();
 
 export default function BatchimScreen() {
   return <HangulLessonScreen moduleId="hangul_batchim" />;
